f435f6e7e3736e3e59b5d2f8c88a5ffd153779b8
abrar abdelhadi <abrara26904@gmail.com>
2026-03-15 14:04:24 +0100

    update

--- app.js
+++ app.js
@@ -1,65 +1,46 @@
 // Q1
-
+let num1 = prompt("Q1: Enter a number to reverse:");
 function reverseNumber(x){
-    let reversed = x.toString().split("").reverse().join("");
-    return Number(reversed);
+    return Number(x.toString().split("").reverse().join(""));
 }
+alert("Q1 Result: " + reverseNumber(num1));
 
-console.log("Q1 Result:", reverseNumber(532443));
-
-
-// ===================================
 
 // Q2
-
+let loopResults = "";
 for(let i = 0; i <= 15; i++){
-    console.log(i + (i % 2 === 0 ? " is even" : " is odd"));
+    loopResults += i + (i % 2 === 0 ? " is even\n" : " is odd\n");
 }
+alert("Q2 Results:\n" + loopResults);
 
-
-// ===================================
 
 // Q3
-
-
-function sortString(str){
-    return str.toLowerCase().split("").sort().join("");
+let str = prompt("Q3: Enter a string to sort alphabetically:");
+function sortString(s){
+    return s.toLowerCase().split("").sort().join("");
 }
+alert("Q3 Result: " + sortString(str));
 
-console.log("Q3 Result:", sortString("Orange"));
-
-
-// ===================================
 
 // Q4
-
-
+let num2 = prompt("Q4: Enter a number to insert dashes between even digits:");
 function insertDash(num){
     let str = num.toString();
     let result = "";
-
     for(let i = 0; i < str.length; i++){
         result += str[i];
-
         if(str[i] % 2 === 0 && str[i+1] % 2 === 0){
             result += "-";
         }
     }
-
     return result;
 }
+alert("Q4 Result: " + insertDash(num2));
 
-console.log("Q4 Result:", insertDash(25468));
-
-
-// ===================================
 
 // Q5
-
-
+let age = prompt("Q5: Enter your age:");
 function Agechecker(age){
-    console.log(age >= 18 ? "The user is Adult" : "The user is Minor");
+    alert(age >= 18 ? "The user is Adult" : "The user is Minor");
 }
-
-Agechecker(20);
-Agechecker(15);
+Agechecker(age);
